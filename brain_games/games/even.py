899443318049f7d97fc0ba91260e6bd724975e50
Dmitriy Malinovskiy <dmalinkos@gmail.com>
2020-05-31 14:46:01 +0300

    fix lint errors

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,23 +8,21 @@ def game_even():
     name = cli.welcome_user()
     step_for_win = 3
     for step in range(step_for_win):
-        number = random.randint(0,100)
-        print(f'Question: {number}')
-        answer =  cli.get_answer()        
-        if  number % 2 == 0:
-            if answer != 'yes':
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
+        num = random.randint(0, 100)
+        print(f'Question: {num}')
+        ans = cli.get_answer()
+        if num % 2 == 0:
+            if ans != 'yes':
+                print(f"'{ans}' is wrong answer ;(. Correct answer was 'yes'.")
                 print("Let's try again, Bill!")
                 return False
             else:
                 print('Correct!')
-        if  number % 2 == 1:
-            if answer != 'no':
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.")
+        if num % 2 == 1:
+            if ans != 'no':
+                print(f"'{ans}' is wrong answer ;(. Correct answer was 'no'.")
                 print(" Let's try again, Bill!")
                 return False
             else:
                 print('Correct!')
     print(f'Congratulations, {name}!')
-
-
