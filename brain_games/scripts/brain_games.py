dddf97ed5c6e9ed7d3c367d8172362abdf58c2c9
Dmitriy Malinovskiy <dmalinkos@gmail.com>
2020-05-31 12:34:28 +0300

    publish 0.2.0 on Test PyPI

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+import brain_games.cli as cli
+
+
 def main():
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!\n')
+    cli.welcome_user()    
 
 
 if __name__ == '__main__':
